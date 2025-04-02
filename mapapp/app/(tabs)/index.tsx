1023236f4627a706c8bc3e66dc8c37092644da68
kevin200301 <99510924+kevin200301@users.noreply.github.com>
2025-04-01 21:18:13 -0400

    some ui built

---
+++ mapapp/app/(tabs)/index.tsx
@@ -0,0 +1,26 @@
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text>test text.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center', 
+  },
+
+  text: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+    margin: 10,
+  }
+});
